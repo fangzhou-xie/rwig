d24e5ab3909a28b07fcccbd74c63e6c1ebb43eec
fangzhou-xie <fangzhou.xie@rutgers.edu>
2025-11-29 09:43:39 -0500

    pin armadillo version

--- inst/include/common.hpp
+++ inst/include/common.hpp
@@ -3,15 +3,20 @@
 #ifndef WIG_COMMON_H
 #define WIG_COMMON_H
 
-#ifndef ARMA_DONT_USE_OPENMP
-#define ARMA_DONT_USE_OPENMP
-#endif
+// #ifndef ARMA_DONT_USE_OPENMP
+// #define ARMA_DONT_USE_OPENMP
+// #endif
 
-#define RCPP_NO_SUGAR
 #define RCPP_ARMADILLO_RETURN_ANYVEC_AS_VECTOR
 
-#include <RcppArmadillo.h>
-// #include <RcppArmadillo/Lightest>
+#define RCPP_NO_SUGAR
+#define RCPP_NO_MODULES
+// #include <RcppArmadillo.h>
+
+
+#include <RcppArmadillo/Lightest>
+
+
 // [[Rcpp::depends(RcppArmadillo)]]
 
 
