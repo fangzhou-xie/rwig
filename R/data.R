a9d3452af3efbaca8e0eb61868f5ed1e7eeadc19
fangzhou-xie <fangzhou.xie@rutgers.edu>
2025-12-02 17:03:01 -0500

    add reference to nyt data

--- R/data.R
+++ R/data.R
@@ -7,4 +7,11 @@
 #'   \item{ref_date}{Date of the News}
 #' }
 #' @source www.nytimes.com
+#' @references
+#'
+#' Xie, F. (2020). Wasserstein index generation model: Automatic generation of
+#' time-series index with application to economic policy uncertainty.
+#' *Economics Letters*, 186, 108874.
+#' https://doi.org/10.1016/j.econlet.2019.108874
+#'
 "headlines"
